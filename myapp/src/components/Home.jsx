5c3a1566bbb323842e8f102207136e6cf4e6666e
taovc <tao.weijie@epitech.eu>
2023-02-09 18:50:28 +0100

    [rm] Login component in Home component, create a new Login component

--- myapp/src/components/Home.jsx
+++ myapp/src/components/Home.jsx
@@ -3,47 +3,6 @@ import { Container, Row, Col } from "react-bootstrap";
 import axios from "axios";
 import { Link, useNavigate } from "react-router-dom";
 
-const Login = () => {
-  const [user, setUser] = useState({
-    email: "",
-    password: "",
-  });
-  const [error, setError] = useState("");
-  const navigate = useNavigate();
-
-  const loginHandler = async (e) => {
-    e.preventDefault();
-
-    const config = {
-      header: {
-        "Content-Type": "application/json",
-      },
-    };
-
-    try {
-      const { data } = await axios.post(
-        `${global.API_ENDPOINT}/api/auth/login`,
-        user,
-        config
-      );
-      localStorage.setItem("authToken", data.token);
-      navigate("/");
-    } catch (error) {
-      setError(error?.response?.data?.error);
-      setTimeout(() => {
-        setError("");
-      }, 3 * 1000);
-    }
-  };
-
-  const handleChange = (e) => {
-    setUser({ ...user, [e.target.name]: e.target.value });
-  };
-  return (
-    <div></div>
-  );
-}
-
 function Home () {
   const [user, setUser] = useState({
     email: "",
