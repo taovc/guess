d80ab54662f3fdb72840db0c163c7e2dcb13f6da
taovc <tao.weijie@epitech.eu>
2023-02-09 19:21:39 +0100

    [add] login post handler

--- myapp/src/components/Login.jsx
+++ myapp/src/components/Login.jsx
@@ -1,10 +1,28 @@
 
-import React from "react"
+import React, {useState} from "react"
+import axios from "axios"
 
 export default function Login (props) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log("email", e.target.email.value)
+        const data = {
+            email: email,
+            password: password
+        }
+       axios.post("http://localhost:3000/api/v1/auth/login", data).then((res) => {
+            console.log(res.data)
+        }).catch((err) => {
+            console.log(err)
+        })
+    }
+
   return (
     <div className="Auth-form-container">
-      <form className="Auth-form">
+      <form className="Auth-form" onSubmit={handleSubmit}>
         <div className="Auth-form-content">
           <h3 className="Auth-form-title">Sign In</h3>
           <div className="form-group mt-3">
