ebb08a2126a96c9c53a8e7bcee81125215285cf0
taovc <tao.weijie@epitech.eu>
2023-02-10 14:19:56 +0100

    [fix] token set error in login

--- back/routes/auth.js
+++ back/routes/auth.js
@@ -5,6 +5,6 @@ const authCtrl = require("../controllers/auth");
 router.post("/auth/register", authCtrl.register);
 router.post("/auth/login", authCtrl.login);
 router.put("/auth/passwordreset/:resetToken", authCtrl.resetPassword);
-router.get("/auth/delete/:email", authCtrl.deleteUser);
+//router.get("/auth/delete/:email", authCtrl.deleteUser);
 
 module.exports = router;
--- myapp/src/components/Auth.jsx
+++ myapp/src/components/Auth.jsx
@@ -1,25 +1,23 @@
-import React, { useState, useEffect, createContext } from "react";
+import React, { useEffect, createContext } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 
 export const AuthContext = createContext();
 
 const AuthProvider = (props) => {
-  const [token, setToken] = useState("");
   const navigate = useNavigate();
 
   useEffect(() => {
-    setToken(localStorage.getItem("token"));
+    const token = localStorage.getItem("token");
     if (token) {
       axios.defaults.headers.common["authorization"] = token;
-      navigate("/");
     } else {
       delete axios.defaults.headers.common["authorization"];
       navigate("/login");
     }
-  }, [token, navigate]);
+  }, [navigate]);
 
-  return <AuthContext.Provider value={{ token, setToken }}>{props.children}</AuthContext.Provider>;
+  return <AuthContext.Provider value={{}}>{props.children}</AuthContext.Provider>;
 };
 
 export default AuthProvider;
