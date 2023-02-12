6fe850b11b472324e88b142433e436f1e0fedff3
taovc <tao.weijie@epitech.eu>
2023-02-12 02:10:41 +0100

    [add] message to display notice

--- myapp/src/components/DisplayNotice.jsx
+++ myapp/src/components/DisplayNotice.jsx
@@ -1,17 +1,13 @@
 import React from "react";
 
 const DisplaySuccess = ({ message }) => {
-  return (
-    <span className="w-full inline-block rounded-xl p-1 bg-green-600 text-white text-center mb-4">
-      {message}
-    </span>
-  );
+  return <span className="display-success">{message}</span>;
 };
 
 const DisplayError = ({ message }) => {
   return (
-    <span className="w-full inline-block rounded-xl p-1 bg-red-600 text-white text-center mb-4">
-      <div>{message}</div>
+    <span className="display-error">
+      <b>{message}</b>
     </span>
   );
 };
--- myapp/src/components/Login.jsx
+++ myapp/src/components/Login.jsx
@@ -1,9 +1,10 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 import Form from "./Form";
-import { Button } from "react-bootstrap";
+import { DisplayError } from "./DisplayNotice";
 
 const urlLogin = "http://localhost:8080/api/auth/login";
 const urlRegister = "http://localhost:8080/api/auth/register";
@@ -12,6 +13,8 @@ export default function Login() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [register, setRegister] = useState(false);
+  const [callbackmsg, setCallbackmsg] = useState("");
+  const [isOk, setisOk] = useState(true);
   const navigate = useNavigate();
 
   const handleSubmit = (e, values) => {
@@ -26,11 +29,16 @@ export default function Login() {
         if (res.data.token) {
           localStorage.setItem("token", JSON.stringify(res.data.token));
           localStorage.setItem("user", JSON.stringify(res.data.user));
+          setisOk(true);
           navigate("/");
         }
       })
       .catch((err) => {
-        console.log(err);
+        setCallbackmsg(err?.response?.data?.error);
+        setisOk(false);
+        setTimeout(() => {
+          setisOk(true);
+        }, 3000);
       });
   };
 
@@ -61,8 +69,11 @@ export default function Login() {
 
   return (
     <div className="Auth-form-container">
+      {!isOk && <DisplayError message={callbackmsg} />}
       <Form fields={formFields} title={formTitle} onSubmit={handleSubmit} />
-      <Button className="Auth-form-container-button" onClick={handleRegister}>{!register ? "Sign Up" : "Sign In"}</Button>
+      <Button className="Auth-form-container-button" onClick={handleRegister}>
+        {!register ? "Sign Up" : "Sign In"}
+      </Button>
     </div>
   );
 }
